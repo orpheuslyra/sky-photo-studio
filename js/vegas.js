3ea6979e60c12c2db0d51ebd4666fbc6679f8913
MitsuoKoizumi <m_koizumi@yumemi.co.jp>
2023-06-30 15:34:33 +0900

    sky studioサイトを作成

---
+++ js/vegas.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('#mainimg').vegas({
+        slides: [
+            { src: './images/1.jpg' },	//1枚目の写真指定
+            { src: './images/2.jpg' },	//2枚目の写真指定
+            { src: './images/3.jpg' },	//3枚目の写真指定
+        ],
+		transition: 'fade',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
+		timer: false,				//プログレスバーを非表示に。
+    });
+});
